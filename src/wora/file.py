8a5e84575afb7a556a01b180119f58f0aed3916a
Joseph Diza <josephm.diza@gmail.com>
2021-12-03 10:30:06 -0800

    Add write_file to file module

--- src/wora/file.py
+++ src/wora/file.py
@@ -3,3 +3,8 @@ def read_file(fname: str) -> str:
     res = file.read()
     file.close()
     return res
+
+def write_file(fname: str, content: str) -> str:
+    file = open(fname, 'w')
+    file.write(content)
+    file.close()
